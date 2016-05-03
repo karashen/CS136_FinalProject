d29690054c1890986623da7de812a6f280f5da28
karashen <karashen@college.harvard.edu>
2016-05-02 21:12:01 -0400

    First files

---
+++ makowskyrubin.py
@@ -0,0 +1,9 @@
+import sys
+from pamodel import make_graph
+
+# Initialize parameters of the model
+m = sys.argv[1]
+r = sys.argv[2]
+gamma = sys.argv[3]
+w1 = sys.argv[4]
+
---
+++ pamodel.py
@@ -0,0 +1,4 @@
+# Function to generate a small world graph
+
+def make_graph(n, q):
+	return []

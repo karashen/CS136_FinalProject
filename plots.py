c954c7e592bd8369b5ecef2379a1c391251e9a80
karashen <karashen@college.harvard.edu>
2016-05-03 14:37:39 -0400

    Changed plots

--- plots.py
+++ plots.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
 
+# Changing social radius
+# r = 1
+eq_time = 
+pref_false = 
+discrepancy = 
+
+# r = 2
+eq_time = 
+pref_false = 
+discrepancy = 
+
+# r = 3
+eq_time = 
+pref_false = 
+discrepancy = 
+
+# r = 4
+eq_time = 
+pref_false = 
+discrepancy = 
+
+# r = 5
+eq_time = 
+pref_false = 
+discrepancy = 
+
+# Changing social network size
+
+# Changing gamma
+
+# Changing phi
+
+
 plt.plot([1,2,3,4], [1,4,9,16], '-ro')
 plt.axis([0, 6, 0, 20])
 plt.show()

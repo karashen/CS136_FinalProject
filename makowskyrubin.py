010c78fcca660a6a152d37ac74a65605a4bc0ce8
karashen <karashen@college.harvard.edu>
2016-05-02 21:24:08 -0400

    Added network graph function

--- makowskyrubin.py
+++ makowskyrubin.py
@@ -2,8 +2,20 @@ import sys
 from pamodel import make_graph
 
 # Initialize parameters of the model
-m = sys.argv[1]
-r = sys.argv[2]
-gamma = sys.argv[3]
-w1 = sys.argv[4]
+m = 10
+r = 5
+gamma = 0
+w1 = 0.5
+w2 = 0.5
+w3 = 0.5
+w4 = 0.5
+wN1 = 0.5
+wN2 = 0.5
+wC1 = 0.5
+wC2 = 0.5
+n = 5
+q = 1
+
+# Make social network using PA model
+network = make_graph(m, q)
 
--- pamodel.py
+++ pamodel.py
@@ -1,4 +1,32 @@
 # Function to generate a small world graph
+import random
+import math
+import matplotlib.pyplot as plt
 
-def make_graph(n, q):
-	return []
+def make_graph(numnodes, q):
+
+    graph = [[0 for node in xrange(numnodes)] for node in xrange(numnodes)]
+    degrees = [0 for node in xrange(numnodes)]
+
+    def init_clique(graph, q):
+        for i in xrange(2*q + 1):
+            for j in xrange(2*q + 1):
+                if i != j:
+                    graph[i][j] = 1
+
+    def add_vertex(graph, i):
+        counter = 0
+        while counter < q:
+            vertex = random.randint(0, i)
+            if vertex != i and graph[i][vertex] == 0 and graph[vertex][i] == 0:
+                graph[i][vertex] = 1
+                graph[vertex][i] = 1
+                counter += 1
+
+    def create_graph(graph):
+        init_clique(graph, q)
+        for i in xrange(2*q + 2, numnodes):
+            add_vertex(graph, i)
+        return graph
+
+    return create_graph(graph)
